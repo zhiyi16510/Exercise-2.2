e0362428978e5043c6fea2806512fc2d565170b7
TEOH ZHI YI 278645 <73087918+zhiyi16510@users.noreply.github.com>
2021-04-05 19:42:33 +0800

    Update Main.java

--- Prog2Exercise2/src/ElectricGadget/Main.java
+++ Prog2Exercise2/src/ElectricGadget/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		canon.specPrinter("Canon", "G3010", 'N', 'Y', 480);
 		brother.specPrinter("Brother", "PTP900W", 'N', 'N', 238);
 		
-		System.out.println(" **** WARRANTY CLAIM ON PRINTER ****");
+		System.out.println(" **** WARRANTY CLAIM ON PRINTER **** ");
 		canon.warrantyClaim("Canon", 2, 30);
 		brother.warrantyClaim("Brother", 1, 60);
 		
@@ -22,14 +22,14 @@ public class Main {
 		brother.freeService("Brother", 2, 5);
 		all.maintenance(10, 3);
 		
-		System.out.println(" $$$$ TOTAL AMOUNT $$$$");
+		System.out.println(" $$$$ TOTAL AMOUNT $$$$ ");
 		canon.price("Canon", 3, 480);
 		
 		System.out.println("SPECIFICATIONS FOR AIR CONDITIONER MODELS");
 		samsung.specAC("Samsung", "1.5HP S-Essential AR12TGHAQBUNME", "870 x 270 x 365", 'N', 1450);
 		midea.specAC("Midea", "AAC12AR", "736 x 330 x 380", 'Y', 841);
 		
-		System.out.println(" **** WARRANTY CLAIM ON AIR CONDITIONER ****");
+		System.out.println(" **** WARRANTY CLAIM ON AIR CONDITIONER **** ");
 		samsung.warrantyClaim("Samsung", 2, 30);
 		midea.warrantyClaim("Midea", 1, 30);
 		
@@ -38,7 +38,7 @@ public class Main {
 		midea.freeService("Midea", 2, 5);
 		all.maintenance(10, 3);
 		
-		System.out.println(" $$$$ TOTAL AMOUNT $$$$");
+		System.out.println(" $$$$ TOTAL AMOUNT $$$$ ");
 		midea.price("Midea", 6, 841);
 	}
 
